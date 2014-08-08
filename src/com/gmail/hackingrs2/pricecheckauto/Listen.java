345a052eaa6aad9b5d84c0ee673e2c3eff88d70f
HackingRs <HackingRs@users.noreply.github.com>
2014-08-08 16:19:31 +0300

    Update Listen.java

--- src/com/gmail/hackingrs2/pricecheckauto/Listen.java
+++ src/com/gmail/hackingrs2/pricecheckauto/Listen.java
@@ -20,57 +20,62 @@ public class Listen extends TimerTask {
 		}
 
 		for (String message : messages) {
-			if (!Client.latestAnswered.contains(message)) {
+			if (!Client.latestAnswered.contains(message) && !message.isEmpty()) {
 				parseAndAnswer(message);
 			}
 		}
 	}
 
 	private void parseAndAnswer(String message) {
-		Client.getClient().send(message, false, false);
+		try {
+			Client.getClient().send(message, false, false);
 
-		if (Client.latestAnswered.size() >= 10) {
-			Client.latestAnswered.clear();
-		}
-
-		Client.latestAnswered.add(message);
-
-		String[] c = message.split(":");
-		String s = c[2].trim();
-		String ss = "";
-
-		if (s.startsWith("pc on")) {
-			ss = "pc on";
-		} else if (s.startsWith("pc")) {
-			ss = "pc";
-		} else if (s.startsWith("price check on")) {
-			ss = "price check on";
-		} else if (s.startsWith("pricecheck")) {
-			ss = "pricecheck";
-		} else if (s.startsWith("price check")) {
-			ss = "price check";
-		} else if (s.startsWith("how much is")) {
-			ss = "how much is";
-		} else {
-			return;
-		}
-
-		String[] cc = s.split(ss);
-		String end = cc[1].replaceAll("\\?", "").replaceAll("\\!", "").trim();
-
-		String itemName = "";
-
-		for (Object item : Login.getDatabase().getItems()) {
-			if (end.equals(item.toString())) {
-				itemName = item.toString();
+			if (Client.latestAnswered.size() >= 10) {
+				Client.latestAnswered.clear();
 			}
-		}
 
-		if (!itemName.isEmpty()) {
-			System.out.println(Login.getDatabase().find(itemName).replaceAll("\\[", "").replaceAll("\\]", ""));
-			/*
-			 * try { Client.getClient().post(Login.getDatabase().find(itemName).replaceAll("\\[", "").replaceAll("\\]", "")); } catch (IOException e) { e.printStackTrace(); }
-			 */
+			Client.latestAnswered.add(message);
+
+			String[] c = message.split(":");
+			String s = c[2].trim();
+			String ss = "";
+
+			if (s.startsWith("pc on")) {
+				ss = "pc on";
+			} else if (s.startsWith("pc")) {
+				ss = "pc";
+			} else if (s.startsWith("price check on")) {
+				ss = "price check on";
+			} else if (s.startsWith("pricecheck")) {
+				ss = "pricecheck";
+			} else if (s.startsWith("price check")) {
+				ss = "price check";
+			} else if (s.startsWith("how much is")) {
+				ss = "how much is";
+			} else {
+				return;
+			}
+
+			String[] cc = s.split(ss);
+			String end = cc[1].replaceAll("\\?", "").replaceAll("\\!", "").trim();
+
+			String itemName = "";
+
+			for (Object item : Login.getDatabase().getItems()) {
+				if (end.equals(item.toString())) {
+					itemName = item.toString();
+				}
+			}
+
+			if (!itemName.isEmpty()) {
+				Client.getClient().send(Login.getDatabase().find(itemName).replaceAll("\\[", "").replaceAll("\\]", ""), false, false);
+				/*
+				 * try { Client.getClient().post(Login.getDatabase().find(itemName).replaceAll("\\[", "").replaceAll("\\]", "")); } catch (IOException e) { e.printStackTrace(); }
+				 */
+			}
+			
+		} catch (Exception ex) {
+			Client.getClient().sendError(ex);
 		}
 	}
 }
